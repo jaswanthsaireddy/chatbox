388b9839e20885cf82f7f437aad1416c068c2695
jaswanthsaireddy <pasalajaswanthsair8@gmail.com>
2025-03-03 14:46:04 +0530

    Refactor Chatpage component by removing unused dispatch and dark mode toggle function

--- src/pages/Chatpage.jsx
+++ src/pages/Chatpage.jsx
@@ -1,17 +1,11 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector} from "react-redux";
 import Chat from "../components/Chat";
-import { storeConversation, toggleDarkMode } from "../redux/chatSlice"; 
 import Sidebar from "../components/Sidebar";
 import ChatFeedback from "../components/ChatFeedback";
 
 function Chatpage() {
-  const dispatch = useDispatch();
+  
   const darkMode = useSelector((state) => state.chat.darkMode); // Get dark mode state from Redux
-
-  const handleToggleDarkMode = () => {
-    dispatch(toggleDarkMode());
-  };
-
   return (
     <div className={`flex h-screen ${darkMode ? 'bg-gray-900' : 'bg-white'}`}>
       <Sidebar />
